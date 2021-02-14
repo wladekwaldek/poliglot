b978946dbf4626d78023fa22025ee70b05d7cefc
wladekwaldek <60227357+wladekwaldek@users.noreply.github.com>
2021-02-14 11:58:41 +0300

    add gtag.js

--- client/src/components/allcontent/content/words/TableRow.jsx
+++ client/src/components/allcontent/content/words/TableRow.jsx
@@ -4,19 +4,19 @@ import {useHttp} from "../../../../hooks/req";
 import {AuthContext} from "../../../../context/authContext";
 
 const TableRow = (props) => {
-    const [word, setWord] = useState({len: '', org: '', rus: ''})
+    //const [word, setWord] = useState({len: props.values.len, org: props.values.org, rus: props.values.rus})
     const {request} = useHttp()
     const auth = useContext(AuthContext)
 
-    useEffect(useCallback(() => {
-        setWord({len: props.values.len, org: props.values.org, rus: props.values.rus})
-    }, []), [props])
+    //useEffect(useCallback(() => {
+    //    setWord({len: props.values.len, org: props.values.org, rus: props.values.rus})
+    //}, []), [props])
 
     const addWords = async ()=>{
         if (!auth.isAuthenticated) {
             alert('Авторизуйтесь, чтобы добавлять слова в словарь.')
         } else {
-            const data = await request('/api/add-words', 'POST', {...word, id: auth.userId}, {})
+            const data = await request('/api/add-words', 'POST', {...props.values, id: auth.userId}, {})
             alert(data.message)
         }
     }
@@ -27,8 +27,8 @@ const TableRow = (props) => {
                 <div className={classes.plus}
                      onClick={addWords}
                 title='добавить в словарь?'>+</div>
-                {word.org}</td>
-            <td>{word.rus}</td>
+                {props.values.org}</td>
+            <td>{props.values.rus}</td>
         </tr>
     )
 }
